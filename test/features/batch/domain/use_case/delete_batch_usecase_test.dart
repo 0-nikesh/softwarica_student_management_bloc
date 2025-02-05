6fcde41cea7c4451bd886573f7dce8bc3b5e3356
nikesh <bhandarinikesh93@gmail.com>
2025-02-05 11:55:13 +0545

    test upto deleted

--- test/features/batch/domain/use_case/delete_batch_usecase_test.dart
+++ test/features/batch/domain/use_case/delete_batch_usecase_test.dart
@@ -1,7 +1,50 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:softwarica_student_management_bloc/features/batch/domain/use_case/delete_batch_usecase.dart';
+
+import 'repository.mock.dart';
+import 'token.mock.dart';
 
 void main() {
-  testWidgets('delete batch usecase ...', (tester) async {
-    // TODO: Implement test
+  late DeleteBatchUsecase usecase;
+  late MockBatchRepository repository;
+  late MockTokenSharedPrefs tokenSharedPrefs;
+
+  setUp(() {
+    repository = MockBatchRepository();
+    tokenSharedPrefs = MockTokenSharedPrefs();
+    usecase = DeleteBatchUsecase(
+      batchRepository: repository,
+      tokenSharedPrefs: tokenSharedPrefs,
+    );
+  });
+
+  final tBatchId = '1';
+  final token = 'token';
+  final deleteBatchParams = DeleteBatchParams(batchId: tBatchId);
+
+  test('delete batch using id', () async {
+    when(() => tokenSharedPrefs.getToken()).thenAnswer(
+      (_) async => Right(token),
+    );
+
+    when(() => repository.deleteBatch(any(), any())).thenAnswer(
+      (_) async => Right(null),
+    );
+
+    //act
+    final result = await usecase(deleteBatchParams);
+
+    //assert
+    expect(result, Right(null));
+
+    //verify
+    verify(() => tokenSharedPrefs.getToken()).called(1);
+    verify(() => repository.deleteBatch(tBatchId, token)).called(1);
+
+    verifyNoMoreInteractions(repository);
+
+    verifyNoMoreInteractions(tokenSharedPrefs);
   });
 }
--- test/features/batch/domain/use_case/get_all_batch_usecase_test.dart
+++ test/features/batch/domain/use_case/get_all_batch_usecase_test.dart
@@ -1,7 +1,44 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:softwarica_student_management_bloc/features/batch/domain/entity/batch_entity.dart';
+import 'package:softwarica_student_management_bloc/features/batch/domain/use_case/get_all_batch_usecase.dart';
+
+import 'repository.mock.dart';
 
 void main() {
-  testWidgets('get all batch usecase ...', (tester) async {
-    // TODO: Implement test
+  late MockBatchRepository repository;
+  late GetAllBatchUseCase usecase;
+
+  setUp(() {
+    repository = MockBatchRepository();
+    usecase = GetAllBatchUseCase(batchRepository: repository);
+  });
+
+  final tBatch = BatchEntity(
+    batchName: 'Test Batch',
+    batchId: '1',
+  );
+
+  final tBatch2 = BatchEntity(
+    batchName: 'Test Batch 2',
+    batchId: '2',
+  );
+
+  final tBatches = [tBatch, tBatch2];
+
+  test('should get batches from repository', () async {
+    when(() => repository.getBatches()).thenAnswer(
+      (_) async => Right(tBatches),
+    );
+
+    //act
+    final result = await usecase();
+
+    //assert
+    expect(result, Right(tBatches));
+
+    //verify
+    verify(() => repository.getBatches()).called(1);
   });
 }
