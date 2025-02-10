355c7a214b2f74952f32b7271fa2f2145cdce4b9
nikesh <bhandarinikesh93@gmail.com>
2025-02-10 11:50:58 +0545

    login widget testing

---
+++ test/features/auth/presentation/view/login_view_test.dart
@@ -0,0 +1,50 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:softwarica_student_management_bloc/features/auth/presentation/view/login_view.dart';
+import 'package:softwarica_student_management_bloc/features/auth/presentation/view_model/login/login_bloc.dart';
+
+class MockLoginBloc extends MockBloc<LoginEvent, LoginState>
+    implements LoginBloc {}
+
+void main() {
+  late MockLoginBloc loginBloc;
+  setUp(() {
+    loginBloc = MockLoginBloc();
+  });
+
+  Widget loadLoginView() {
+    return BlocProvider<LoginBloc>(
+        create: (context) => loginBloc,
+        child: MaterialApp(
+          home: LoginView(),
+        ));
+  }
+
+  testWidgets('check for the text in login Ui', (tester) async {
+    await tester.pumpWidget(loadLoginView());
+
+    await tester.pumpAndSettle();
+
+    //find button by text
+    final result = find.widgetWithText(ElevatedButton, 'Login');
+    final result2 = find.widgetWithText(ElevatedButton, 'Register');
+    expect(result, findsOneWidget);
+    expect(result2, findsOneWidget);
+  });
+
+  testWidgets('Check for the username and password', (tester) async {
+    await tester.pumpWidget(loadLoginView());
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextField).at(0), 'nikesh');
+    await tester.enterText(find.byType(TextField).at(1), 'nikesh123');
+
+    await tester.tap(find.byType(ElevatedButton).first);
+
+    await tester.pumpAndSettle();
+    expect(find.text('nikesh'), findsOneWidget);
+    expect(find.text('nikesh123'), findsOneWidget);
+  });
+}
